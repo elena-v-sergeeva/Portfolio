572aac935efc50ac292261365e75c2b496115294
Elena <mediagold@mail.ru>
2020-12-25 02:54:09 +0300

    Fifth commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -77,4 +77,94 @@ $(document).ready(function () {
 		}
 	});
 
+	// form placeholder
+	const formItems = document.querySelectorAll('.form-field');
+	
+	for(let item of formItems){
+		const thisParent = item.closest('.form-item');
+		const thisPlaceholder = thisParent.querySelector('.fake-palceholder');
+
+		// Если инпут в фокусе	
+
+		item.addEventListener('focus', function(){
+			thisPlaceholder.classList.add('active');
+		});
+
+		// Если инпут теряет фокус
+
+		item.addEventListener('blur', function(){
+
+			if(item.value.length > 0){
+				thisPlaceholder.classList.add('active');
+			}
+			else{
+				thisPlaceholder.classList.remove('active');
+			}
+		})
+	}
+
+	//FORM VALIDATE
+	$('.contacts-form').validate({
+		rules: {
+			email: {
+				required: true,
+				email: true
+			},
+			subject: {
+				required: true
+			},
+			message: {
+				required: true
+			}
+		},
+		messages: {
+			email: {
+				required: 'Введите email',
+				email: 'Отсутсвует символ @'
+			},
+			subject: {
+				required: 'Введите тему сообщения'
+			},
+			message: {
+				required: 'Введите текст сообщения'
+			}
+		},
+		submitHandler: function (form) {
+			ajaxFormSubmit();
+		}
+
+	})
+
+	// Функция AJAX запрса на сервер
+
+	function ajaxFormSubmit() {
+
+		let string = $(".contacts-form").serialize(); // Соханяем данные введенные в форму в строку.
+
+		//Формируем ajax запрос
+		$.ajax({
+			type: "POST", // Тип запроса - POST
+			url: "php/mail.php", // Куда отправляем запрос
+			data: string, // Какие даные отправляем, в данном случае отправляем переменную string
+
+			// Функция если все прошло успешно
+			success: function (html) {
+				$(".contacts-form").slideUp(800);
+				$('#answer').html(html);
+			}
+		});
+		// Чтобы по Submit больше ничего не выполнялось - делаем возврат false чтобы прервать цепчку срабатывания остальных функций
+		return false;
+	}
+
+	// ПАРАЛЛАКС ДВИЖЕНИЯ ЗА МЫШКОЙ​
+	let prxScene = document.querySelector('.section-contacts')
+	let prxItem = document.querySelectorAll('.move-quot');
+	prxScene.addEventListener('mousemove', function (e) {
+		let x = e.clientX / window.innerWidth;
+		let y = e.clientY / window.innerHeight;
+		for (let item of prxItem) {
+			item.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)';
+		}
+	});
 })	
